8df9cc6a5bf3c5e61d9bae760203bc4c14226ed9
mbouedithmireille <74009314+mbouedithmireille@users.noreply.github.com>
2020-11-05 16:24:35 +0100

    Add files via upload

---
+++ monprofil.php
@@ -0,0 +1,113 @@
+<?php
+
+
+session_start();
+$Name = $_POST['Name'];
+$Company = $_POST['Company'];
+$email =$_POST['email'];
+$password = md5($_POST['password']);
+$confermpassword = md5($_POST['confermpassword']);
+
+   if
+       ( !empty($Name) || !empty($password) || !empty($confermpassword)) 
+   {
+       
+       include "pageperso.php";
+       
+if(mysqli_connect_error())
+{
+die('connect_Error('.mysqli_connect_errno().')'.mysqli_connect_error());
+ }
+   
+       else
+    {   
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)) 
+            {
+                
+               $SELECT ="SELECT Name FROM profil WHERE Name= ? Limit 1";
+                //insert the element in the database
+                
+                $INSERT= "INSERT INTO profil (Name, Company, email, password) VALUES (?,?,?,?)";
+              
+              
+ //prepared statement
+                    
+     $stmt = $conn ->prepare ($SELECT);
+     $stmt -> bind_param("i", $Name);
+     $stmt -> execute();
+     $stmt ->store_result();
+     $rnum =$stmt -> num_rows;
+                    
+    if($rnum == 0)
+        {
+ $SELECTE ="SELECT email FROM profil WHERE email= ? Limit 1";          
+     $stmt = $conn ->prepare ($SELECTE);
+     $stmt -> bind_param("s", $email);
+     $stmt -> execute();
+     $stmt ->store_result();
+     $rnume =$stmt -> num_rows    
+            
+            
+     $conn->query($INSERT);          
+   
+    
+require 'Phpmailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
+$mail ->Host ='smtp.gmail.com';
+$mail->port=587;
+$mail->SMPTPAuth =true;
+$mail->SMTPSecure='tls';
+$mail->Username='site43244@gmail.com';
+$mail->Password ='Qwerty.1234';
+//echo   $verification_mail['email'];
+$mail->setFrom($_POST['email']);
+$mail->addAddress($_POST['email']);
+$mail->addReplyTo('site43244@gmail.com');
+$mail->isHTML(true);
+$mail->Subject ='conferm registration';
+$mail->Body ="your registration have sucessfullu done";
+if(!$mail->send())
+{
+    echo "we have a problem to send the mail";
+}
+else
+{
+   echo "we have send a mail to your address"; 
+}
+
+                       
+        echo "we have create the account !" ;
+          
+                     
+                  } 
+                   
+                   
+               }
+        else
+        {
+            echo 'we had already use this mail';
+        }
+    }
+                else
+                   { 
+                   
+                     echo "questo pseudo or name è già stato usato ";
+                  
+               }
+               
+                
+            } 
+             else {
+              
+                echo "please enter the item in the place";
+                
+            }
+             
+             
+          {
+              $stmt ->close();
+              $conn -> close();
+          }
+      
+   
+?>
---
+++ pageperso.php
@@ -0,0 +1,12 @@
+ 
+<?php
+// conection wiith database
+        $host ="localhost";
+       $dbuser="S4412388";
+       $dbpassword="Jouer.ballon";
+       $dbname="S4412388";
+       
+       //creation of the connection
+       
+$conn =  mysqli_connect($host,$dbuser,$dbpassword,$dbname);
+?>
